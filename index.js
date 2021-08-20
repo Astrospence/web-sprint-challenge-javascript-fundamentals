cdc65cef406598811a3563376d44b0d449335f61
Astrospence <illustrator.wood@gmail.com>
2021-08-20 11:16:33 -0600

    finished request 3

--- index.js
+++ index.js
@@ -94,9 +94,14 @@ const zooAnimals = [
   Using lowPopulationAnimals use .filter() to create a new array of objects which contains only the animals with a population of less than 5.
   */
 
-  function lowPopulationAnimals(/*Your Code Here*/){
-    /*Your Code Here*/
+  function lowPopulationAnimals(array){
+      const lowPop = array.filter(function(item){
+        return item.population < 5;
+      });
+      return lowPop;
   }
+
+  console.log('Request 3:', lowPopulationAnimals(zooAnimals));
   
 
   /* 🦁🦁🦁 Request 4: .reduce() 🦁🦁🦁
